e02de00065bd931541e7b563aaeb927cceb0a417
Rita <12490147+rlaw125@users.noreply.github.com>
2019-12-05 10:19:04 -0500

    Updated function

--- ip_address_counter.py
+++ ip_address_counter.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
+#/usr/bin/env python3
 
 import sys
 import re
-
-# Uses regular expressions to match IP addresses and counts the total number in test file
+# Uses regular expressions to match IP addresses and counts the frequency of appearance
 # Use the test file ip_address_counter.txt
 
 def display_IP(key, list_name):
-    print key + ' - ' + str(list_name[key
+    return key + ' - ' + str(list_name[key])
 
 def count_IPs(filename):
     file_content = open(filename, 'r')
@@ -16,20 +15,22 @@ def count_IPs(filename):
 
     for line in content:
         match = re.findall(r'^[\d]{1,3}.[\d]{1,3}.[\d]{1,3}.[\d]{1,3}', line)
-        match = ''.join(match) 
+        match = ''.join(match)
         if not match in unique_ip:
             unique_ip[match] = 1
-        else: 
+        else:
             unique_ip[match]+=1
 
     sorted_IP = sorted(unique_ip.keys())
+    new_list=[]
     for key in sorted_IP:
-        display_IP(key, unique_ip)
+        new_list.append(display_IP(key, unique_ip))
 
+    return new_list
 
 def main():
     first_arg = sys.argv[1]
-    count_IPs(first_arg)
+    print(*count_IPs(first_arg), sep='\n')
 
 
 if __name__ == '__main__':
